0eb776f5f5bb40f9bc4cc3ef94eb074de382751e
jake currie <jakecurrie1@gmail.com>
2024-12-02 22:09:05 -0800

    ya

--- lib/infrastructure-stack.ts
+++ lib/infrastructure-stack.ts
@@ -1,16 +1,117 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as dotenv from 'dotenv';
 
-export class InfrastructureStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+dotenv.config();
+
+const required_env = [
+  'APP_REPO_OWNER',
+  'APP_REPO_NAME',
+  'APP_REPO_BRANCH',
+  'GITHUB_TOKEN'
+];
+
+for (const env of required_env) {
+  if (!process.env[env]) {
+    throw new Error(`Missing required environment variable: ${env}`);
+  }
+}
+
+export class StaticSiteStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const websiteBucket = new s3.Bucket(this, 'WebsiteBucket', {
+      publicReadAccess: true,
+      websiteIndexDocument: 'index.html',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+      accessControl: s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfrastructureQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const buildProject = new codebuild.PipelineProject(this, 'BuildProject', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+      },
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          install: {
+            commands: [
+              'npm install'
+            ]
+          },
+          build: {
+            commands: [
+              'npm run build'
+            ]
+          }
+        },
+        artifacts: {
+          'base-directory': 'dist',
+          'files': [
+            '**/*'
+          ],
+          'discard-paths': 'no'
+        }
+      }),
+    });
+
+    buildProject.addToRolePolicy(new iam.PolicyStatement({
+      actions: ['s3:PutObject', 's3:GetObject', 's3:ListBucket'],
+      resources: [websiteBucket.bucketArn, `${websiteBucket.bucketArn}/*`]
+    }));
+
+    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+      pipelineName: 'StaticSitePipeline',
+    });
+
+    const sourceOutput = new codepipeline.Artifact();
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [
+        new codepipeline_actions.GitHubSourceAction({
+          actionName: 'GitHub',
+          owner: process.env.APP_REPO_OWNER!,
+          repo: process.env.APP_REPO_NAME!,
+          branch: process.env.APP_REPO_BRANCH!,
+          oauthToken: cdk.SecretValue.plainText(process.env.GITHUB_TOKEN!),
+          output: sourceOutput,
+        }),
+      ],
+    });
+
+    const buildOutput = new codepipeline.Artifact();
+    pipeline.addStage({
+      stageName: 'Build',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'CodeBuild',
+          project: buildProject,
+          input: sourceOutput,
+          outputs: [buildOutput],
+        }),
+      ],
+    });
+
+    pipeline.addStage({
+      stageName: 'Deploy',
+      actions: [
+        new codepipeline_actions.S3DeployAction({
+          actionName: 'S3Deploy',
+          bucket: websiteBucket,
+          input: buildOutput,
+        }),
+      ],
+    });
+
+    new cdk.CfnOutput(this, 'WebsiteURL', {
+      value: websiteBucket.bucketWebsiteUrl,
+      description: 'The URL of the website',
+    });
   }
 }
